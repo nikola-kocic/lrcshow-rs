443e335b387d35e2427011df14e82de75f911ce8
Nikola Kocic <nikola-kocic@users.noreply.github.com>
2019-11-06 00:17:15 +0100

    Support lrc files with multiple time tags in line

--- src/lrc.rs
+++ src/lrc.rs
@@ -1,10 +1,12 @@
 use std::fmt;
 use std::time::Duration;
 
+use std::convert::TryInto;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::str::Chars;
+
+use log::debug;
 
 fn lines_from_file<P: AsRef<Path>>(filepath: P) -> Result<Vec<String>, String> {
     let file = File::open(filepath).map_err(|e| e.to_string())?;
@@ -51,23 +53,21 @@ pub struct LrcFile {
     pub timed_texts_lines: Vec<TimedText>,
 }
 
-fn duration_from_time_string(
-    chars: &mut dyn std::iter::Iterator<Item = char>,
-) -> Result<Duration, String> {
-    let minutes_str: String = chars.take(2).collect();
+fn duration_from_time_string(time_str: &str) -> Result<Duration, String> {
+    let minutes_str = &time_str[0..2];
     let minutes = u64::from_str_radix(&minutes_str, 10).expect("Bad minutes format");
 
-    if chars.next() != Some(':') {
+    if &time_str[2..3] != ":" {
         return Err(String::from("Bad seconds divider"));
     }
-    let seconds_str: String = chars.take(2).collect();
+    let seconds_str = &time_str[3..5];
     let seconds = u64::from_str_radix(&seconds_str, 10).expect("Bad seconds format");
 
-    let ms_divider_char = chars.next();
-    if ms_divider_char != Some('.') && ms_divider_char != Some(':') {
+    let ms_divider_char = &time_str[5..6];
+    if ms_divider_char != "." && ms_divider_char != ":" {
         return Err(String::from("Bad milliseconds divider"));
     }
-    let centiseconds_str: String = chars.take(2).collect();
+    let centiseconds_str = &time_str[6..8];
     let centiseconds = u64::from_str_radix(&centiseconds_str, 10).expect("Bad centiseconds format");
 
     Ok(Duration::from_micros(
@@ -75,31 +75,63 @@ fn duration_from_time_string(
     ))
 }
 
-fn parse_lrc_line(chars: &mut std::iter::Peekable<Chars>) -> Result<LrcLine, String> {
-    match chars.next() {
+enum Tag {
+    Time(std::time::Duration),
+    Unknown,
+}
+
+fn parse_tag(tag_content: &str) -> Result<Tag, String> {
+    debug!("Parsing tag content {}", tag_content);
+    let first_char_in_tag_name = tag_content.chars().next().expect("Invalid lrc file format");
+    if first_char_in_tag_name.is_ascii_digit() {
+        let time = duration_from_time_string(tag_content)?;
+        Ok(Tag::Time(time))
+    } else {
+        Ok(Tag::Unknown)
+    }
+}
+
+fn parse_lrc_line(line: String) -> Result<LrcLine, String> {
+    debug!("Parsing line {}", line);
+    match line.chars().next() {
         None => Ok(LrcLine::Empty),
         Some('[') => {
-            let first_char_in_tag_name = chars.peek().expect("Invalid lrc file format");
-            if first_char_in_tag_name.is_ascii_digit() {
-                let time = duration_from_time_string(&mut chars.take_while(|c| *c != ']'))?;
-                if chars.next() != Some(']') {
-                    return Err(String::from("Invalid lrc file format"));
+            let mut current_text_index_in_line = 0;
+            let parts = line.split('[');
+            let mut timings = Vec::new();
+            let mut texts = Vec::new();
+            for part in parts.skip(1) {
+                let mut subparts = part.split(']');
+                let tag_content = subparts.next().unwrap();
+                let mut text_len: i32 = 0;
+
+                if let Some(text) = subparts.next() {
+                    texts.push(text);
+                    text_len = text.bytes().len().try_into().unwrap();
                 }
-                let text: String = chars.collect();
-                let text_len = text.chars().count() as i32;
-                let mut timings = Vec::new();
-                let location = TimedLocation {
-                    time,
-                    line_char_from_index: 0,
-                    line_char_to_index: text_len,
-                };
-                timings.push(location);
-                Ok(LrcLine::TimedText(TimedText { text, timings }))
-            } else {
-                Ok(LrcLine::Unknown)
+
+                match parse_tag(tag_content)? {
+                    Tag::Time(time) => {
+                        let location = TimedLocation {
+                            time,
+                            line_char_from_index: current_text_index_in_line,
+                            line_char_to_index: current_text_index_in_line + text_len,
+                        };
+                        timings.push(location);
+                        current_text_index_in_line += text_len;
+                    }
+                    Tag::Unknown => {
+                        return Ok(LrcLine::Unknown);
+                    }
+                }
             }
+            let text = texts.join("");
+            Ok(LrcLine::TimedText(TimedText { text, timings }))
         }
-        _ => Err(String::from("Invalid lrc file format")),
+        Some(c) => Err(format!(
+            "Invalid lrc file format. First character in line: {}",
+            c
+        )),
     }
 }
 
@@ -107,8 +139,7 @@ pub fn parse_lrc_file<P: AsRef<Path>>(filepath: P) -> Result<LrcFile, String> {
     let text_lines = lines_from_file(filepath)?;
     let mut timed_texts_lines = Vec::new();
     for line in text_lines {
-        let mut chars = line.chars().peekable();
-        let lrc_line = parse_lrc_line(&mut chars)?;
+        let lrc_line = parse_lrc_line(line)?;
         if let LrcLine::TimedText(t) = lrc_line {
             timed_texts_lines.push(t);
         }
--- src/lyrics-signal-receiver.py
+++ src/lyrics-signal-receiver.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
+import sys
+
 import gobject
 
 import dbus
 import dbus.mainloop.glib
 
 lyrics_text = None
+last_line_index = -1
 
 def read_lyrics():
     global lyrics_text
@@ -16,16 +19,23 @@ def read_lyrics():
     print(lyrics_text)
 
 def on_active_lyrics_line_changed(line_index, line_char_from_index, line_char_to_index):
+    global last_line_index
     if lyrics_text is None:
         read_lyrics()
-    print(line_index, line_char_from_index, line_char_to_index)
-    print(lyrics_text[line_index][line_char_from_index:line_char_to_index])
+    # print(line_index, line_char_from_index, line_char_to_index)
+    if line_index > last_line_index:
+        sys.stdout.write("\r{}".format(" " * 80))
+        sys.stdout.write("\r{}".format(lyrics_text[line_index]))
+        last_line_index = line_index
+        print()
+    sys.stdout.write("\r{}{}".format('-' * line_char_from_index, '^' * (line_char_to_index - line_char_from_index)))
+    sys.stdout.flush()
 
 if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     bus = dbus.SessionBus()
-
+    print()
     if lyrics_text is None:
         read_lyrics()
 
