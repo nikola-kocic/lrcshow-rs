f2e24dfcbdcd0ce323054daf52bac845f0f23c6a
Nikola Kocic <nikola-kocic@users.noreply.github.com>
2019-11-05 22:34:04 +0100

    Improve lyrics-signal-receiver.py

--- src/lyrics-signal-receiver.py
+++ src/lyrics-signal-receiver.py
@@ -5,13 +5,29 @@ import gobject
 import dbus
 import dbus.mainloop.glib
 
+lyrics_text = None
+
+def read_lyrics():
+    global lyrics_text
+    hal_manager_object = bus.get_object('com.github.nikola_kocic.lrcshow_rs', '/com/github/nikola_kocic/lrcshow_rs/Lyrics')
+    hal_manager_interface = dbus.Interface(hal_manager_object, 'com.github.nikola_kocic.lrcshow_rs.Lyrics')
+    lyrics_text_raw = hal_manager_interface.GetCurrentLyrics()
+    lyrics_text = [str(x) for x in lyrics_text_raw]
+    print(lyrics_text)
+
 def on_active_lyrics_line_changed(line_index, line_char_from_index, line_char_to_index):
+    if lyrics_text is None:
+        read_lyrics()
     print(line_index, line_char_from_index, line_char_to_index)
+    print(lyrics_text[line_index][line_char_from_index:line_char_to_index])
 
 if __name__ == '__main__':
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
     bus = dbus.SessionBus()
+
+    if lyrics_text is None:
+        read_lyrics()
 
     bus.add_signal_receiver(
         on_active_lyrics_line_changed,
